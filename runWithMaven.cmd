65186f3fa5c5f5c0569780e6745c48fe1209e674
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-11-22 00:01:55 +0400

    explicit charset set (fixes wind default encoding)

--- runWithMaven.cmd
+++ runWithMaven.cmd
@@ -1,2 +1,2 @@
-mvn compile exec:java -Dexec.mainClass=mephi.cybern223.RestaurantController
+mvn -Dfile.encoding=UTF-8 compile exec:java -Dexec.mainClass=mephi.cybern223.RestaurantController
 
--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-java -jar kostyaev_pomadchin.jar
+java -Dfile.encoding=UTF-8 -jar kostyaev_pomadchin.jar
